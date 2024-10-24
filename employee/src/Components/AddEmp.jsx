8029965bb160eb043407ad672d379ff911436b5c
ittirarichan <abhishekbinish86@gmail.com>
2024-10-24 11:31:52 +0530

    almost done

--- employee/src/Components/AddEmp.jsx
+++ employee/src/Components/AddEmp.jsx
@@ -2,46 +2,125 @@ import React, { useState} from 'react';
 import axios from 'axios';
 
 const AddEmp = () => {
-    const [title,setTitle] = useState('');
-    const [description, setDescription] = useState('');
+    const [empid,setEmpid] = useState('');
+    const [name, setName] = useState('');
+    const [address, setAddress] = useState('');
+    const [position, setPosition] = useState('');
+    const [salary, setSalary] = useState('');
+    const [experiance, setExperiance] = useState('');
+    const [phone, setPhone] = useState('');
+    const [email, setEmail] = useState('');
+
+    const handleClick =() =>{
+        //force a page reload
+        window.location.reload();
+    };
     
     const handleSubmit = (e) => {
         e.preventDefault();
-        axios.post('https://aiswarya2325.pythonanywhere.com/employemanagement/employees/',{title, description})
+        axios.post('https://aiswarya2325.pythonanywhere.com/employemanagement/employees/',{ empid, name, address, position, salary, experiance, phone, email })
             .then(response => {
                 console.log(response.data);
-                setTitle('');
-                setDescription('');
+                setEmpid('');
+                setName('');
+                setAddress('');
+                setPosition('');
+                setSalary('');
+                setExperiance('');
+                setPhone('');
+                setEmail('');
             })
             .catch(error => console.log(error));
+            handleClick()
 }
 
 
 return(
-    <div className='container'>
+    <div className='container w-50'>
     <form onSubmit={handleSubmit}>
         <h2 className='text-info'><u>Add Employee</u></h2>
 
         <div>
-            <label className='text-dark'>Title</label>
+            <label className='text-dark'>Emp Id</label>
+            <input 
+            className='form-control'
+            type='number'
+            value={empid}
+            onChange={(e) => setEmpid(e.target.value)} 
+            />
+        </div>
+
+        <div className='text-dark mt-2'>
+            <label >Name</label>
             <input 
             className='form-control'
             type='text'
-            value={title}
-            onChange={(e) => setTitle(e.target.value)} 
+            value={name}
+            onChange={(e) => setName(e.target.value)} 
             />
         </div>
 
         <div className='mt-2'>
-            <label> Description</label>
+            <label> Address</label>
             <textarea
                 className='form-control'
-                value={description}
-                onChange={(e) => setDescription(e.target.value)}            
+                value={address}
+                onChange={(e) => setAddress(e.target.value)}            
             />
-
         </div>
-        <button type="submit" className="btn btn-success mt-3">Add Task</button>
+
+        <div className='text-dark mt-2'>
+            <label>Position</label>
+            <input 
+            className='form-control'
+            type='text'
+            value={position}
+            onChange={(e) => setPosition(e.target.value)} 
+            />
+        </div>
+
+        <div className='text-dark mt-2'>
+            <label>Salary</label>
+            <input 
+            className='form-control'
+            type='number'
+            value={salary}
+            onChange={(e) => setSalary(e.target.value)} 
+            />
+        </div>
+
+        <div className='text-dark mt-2'>
+            <label>Experiance</label>
+            <input 
+            className='form-control'
+            type='number'
+            value={experiance}
+            onChange={(e) => setExperiance(e.target.value)} 
+            />
+        </div>
+
+        <div className='text-dark mt-2'>
+            <label>Phone</label>
+            <input 
+            className='form-control'
+            type='number'
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)} 
+            />
+        </div>
+
+        <div className='text-dark mt-2'>
+            <label>Email</label>
+            <input 
+            className='form-control'
+            type='email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)} 
+            />
+        </div>
+
+
+        <button type="submit" className="btn btn-success mt-3">Add Employee</button>
     </form>
     </div>
     );
--- employee/src/Components/EmpList.jsx
+++ employee/src/Components/EmpList.jsx
@@ -5,7 +5,7 @@ import axios from 'axios';
 const EmpList = () => {
     const [employees, setEmployees] = useState([]);
     const [editing, setEditing] = useState(false);
-    const [currentEmployee, setCurrentEmployee] = useState({id:null,name:'',address:''});
+    const [currentEmployee, setCurrentEmployee] = useState({id:null,empid:null,name:'',address:'',position:'',salary:null,experiance:null,phone:null,email:''});
 
 
     useEffect(() => {
@@ -43,16 +43,39 @@ const EmpList = () => {
     return(
         <div className="container mt-3">
             <h2>Employee List</h2>
-            <table className="table table-bordered table-hover">
+            <table className="table table-bordered table-hover text-center">
+                <thead>
+                    <tr>
+                        <td>Empid</td>
+                        <td>Name</td>
+                        <td>Address</td>
+                        <td>Position</td>
+                        <td>Salary</td>
+                        <td>Experiance</td>
+                        <td>Phone</td>
+                        <td>Email</td>
+                        <td>Edit</td>
+                        <td>Delete</td>
+                    </tr>
+                </thead>
+                <tbody>
                 {employees.map(employee => (
                     <tr key={employee.id}>
-                        <td>{employee.id}</td>
+                        <td>{employee.empid}</td>
                         <td>{employee.name}</td>
                         <td>{employee.address}</td>
+                        <td>{employee.position}</td>
+                        <td>{employee.salary}</td>
+                        <td>{employee.experiance}</td>
+                        <td>{employee.phone}</td>
+                        <td>{employee.email}</td>
+                
                         <td><button className="btn btn-warning px-3" onClick={() => editEmployee(employee)}>Edit</button></td>
                         <td><button className="btn btn-danger" onClick={() => deleteEmployee(employee.id)}>Delete</button></td>
                     </tr>
+                    
                 ))}
+                </tbody>
             </table>
             {editing ? (
                 <EditEmployeeForm
@@ -67,24 +90,40 @@ const EmpList = () => {
 
 
 
-const EditEmployeeForm = ({currentemployee, updateemployee}) => {
-    const[employee, setemployee] =useState(currentemployee);
+const EditEmployeeForm = ({currentEmployee, updateEmployee}) => {
+    const[employee, setEmployee] =useState(currentEmployee);
+
+        useEffect( ()=> {
+            setEmployee(currentEmployee)
+        },[currentEmployee])
 
 
 const handleInputChange = (e) => {
     const {name, value} = e.target;
-    setemployee({ ...employee, [name]: value });
+    setEmployee({ ...employee, [name]: value });
 };
 
 const handleSubmit = (e) => {
     e.preventDefault();
-    updateemployee(employee.id, employee);
+    updateEmployee(employee.id, employee);
 };
 return(
     <form onSubmit={handleSubmit}>
         <h2>Edit Employee</h2>
+
         <div>
-            <label>Employee</label>
+            <label>Emp Id</label>
+            <input 
+            type="number"
+            name="empid"
+            value={employee.empid}
+            onChange={handleInputChange}
+            />
+        </div>
+
+
+        <div>
+            <label>Name</label>
             <input 
             type="text"
             name="name"
@@ -92,14 +131,70 @@ return(
             onChange={handleInputChange}
             />
         </div>
+
+
         <div>
-        <label>Discription</label>
+        <label>Address</label>
             <textarea
-            name="discription"
+            name="Address"
             value={employee.address}
             onChange={handleInputChange}
             />
         </div>
+
+        <div>
+            <label>Position</label>
+            <input 
+            type="text"
+            name="position"
+            value={employee.position}
+            onChange={handleInputChange}
+            />
+        </div>
+
+        <div>
+            <label>Salary</label>
+            <input 
+            type="number"
+            name="salary"
+            value={employee.salary}
+            onChange={handleInputChange}
+            />
+        </div>
+
+        <div>
+            <label>Experiance</label>
+            <input 
+            type="number"
+            name="experiance"
+            value={employee.experiance}
+            onChange={handleInputChange}
+            />
+        </div>
+
+        <div>
+            <label>Phone</label>
+            <input 
+            type="number"
+            name="phone"
+            value={employee.phone}
+            onChange={handleInputChange}
+            />
+        </div>
+
+        <div>
+            <label>Email</label>
+            <input 
+            type="email"
+            name="email"
+            value={employee.email}
+            onChange={handleInputChange}
+            />
+        </div>
+
+
+
+
         <button type="submit">Update Employee</button>
     </form>
 )
